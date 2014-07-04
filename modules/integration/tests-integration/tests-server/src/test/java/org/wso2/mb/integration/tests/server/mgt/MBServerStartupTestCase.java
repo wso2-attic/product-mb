997856e954ff2b94c83394bc62e0514a1af5ad75
Asanka Abeyweera <asankaab@wso2.com>
2014-07-04 17:54:01 +0530

    Add Server startup test case

---
+++ modules/integration/tests-integration/tests-server/src/test/java/org/wso2/mb/integration/tests/server/mgt/MBServerStartupTestCase.java
@@ -0,0 +1,6 @@
+package org.wso2.mb.integration.tests.server.mgt;
+
+import org.wso2.carbon.integration.common.tests.ServerStartupBaseTest;
+
+public class MBServerStartupTestCase extends ServerStartupBaseTest{
+}
