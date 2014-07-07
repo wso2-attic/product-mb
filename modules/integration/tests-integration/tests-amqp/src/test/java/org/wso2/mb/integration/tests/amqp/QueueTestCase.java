5b9b9af1f4c028f918e01c4ef6c190db0b3ba842
Asanka Abeyweera <asankaab@wso2.com>
2014-07-07 17:14:52 +0530

    Rename tests-transport to tests-amqp

--- modules/integration/tests-integration/tests-amqp/src/test/java/org/wso2/mb/integration/tests/amqp/QueueTestCase.java
+++ modules/integration/tests-integration/tests-amqp/src/test/java/org/wso2/mb/integration/tests/amqp/QueueTestCase.java
@@ -16,7 +16,7 @@
 * under the License.
 */
 
-package org.wso2.carbon.mb.transport.amqp;
+package org.wso2.mb.integration.tests.amqp;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
