8b4d73859d44d62ea452aa962291f88e503f664c
D.A.U.Nanayakkara <daun07@gmail.com>
2014-07-08 10:12:28 +0530

    Test case to check for deleting a queue from UI is added.

---
+++ modules/integration/tests-ui-integration/src/test/java/org/wso2/carbon/mb/ui/test/queues/QueueDeleteTestCase.java
@@ -0,0 +1,60 @@
+/*
+*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+package org.wso2.carbon.mb.ui.test.queues;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.mb.integration.common.utils.backend.MBIntegrationUiBaseTest;
+import org.wso2.mb.integration.common.utils.ui.Pages.HomePage;
+import org.wso2.mb.integration.common.utils.ui.Pages.LoginPage;
+import org.wso2.mb.integration.common.utils.ui.Pages.QueueAddPage;
+import org.wso2.mb.integration.common.utils.ui.Pages.QueuesBrowsePage;
+
+public class QueueDeleteTestCase extends MBIntegrationUiBaseTest {
+
+    @BeforeClass()
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Test()
+    public void testCase()  throws Exception{
+
+        String qName = "testQ";
+        driver.get(getLoginURL());
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = loginPage.loginAs(mbServer.getContextTenant().getContextUser().getUserName(),
+                mbServer.getContextTenant().getContextUser().getPassword());
+
+
+        QueueAddPage queueAddPage = homePage.getQueueAddPage();
+        Assert.assertEquals(queueAddPage.addQueue(qName), true);
+        QueuesBrowsePage queuesBrowsePage = homePage.getQueuesBrowsePage();
+        Assert.assertEquals(queuesBrowsePage.deleteQueue(qName), true);
+
+    }
+
+    @AfterClass()
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
