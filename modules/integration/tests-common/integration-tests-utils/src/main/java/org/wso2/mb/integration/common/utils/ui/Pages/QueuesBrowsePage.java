295667786641c445b16458be6727f7670de0f74f
Asitha Nanayakkara <daun07@gmail.com>
2014-07-11 14:54:19 +0530

    Queue delete test waiting for elements improved

--- modules/integration/tests-common/integration-tests-utils/src/main/java/org/wso2/mb/integration/common/utils/ui/Pages/QueuesBrowsePage.java
+++ modules/integration/tests-common/integration-tests-utils/src/main/java/org/wso2/mb/integration/common/utils/ui/Pages/QueuesBrowsePage.java
@@ -40,8 +40,8 @@ public class QueuesBrowsePage {
         }
     }
 
-    public boolean isQueuePresent(final String qName){
-        if(getTableRowByQueueName(qName) == null){
+    public boolean isQueuePresent(final String qName) {
+        if (getTableRowByQueueName(qName) == null) {
             return false;
         }
         return true;
@@ -51,7 +51,7 @@ public class QueuesBrowsePage {
 
         boolean isSuccessful = false;
         WebElement row = getTableRowByQueueName(qName);
-        if(row == null) // nothing to delete
+        if (row == null) // nothing to delete
             return isSuccessful;    // return false
 
         List<WebElement> columnList = row.findElements(By.tagName("td"));
@@ -64,8 +64,8 @@ public class QueuesBrowsePage {
 
         // find ok button in popup dialog and click it
         List<WebElement> buttonList = driver.findElements(By.tagName("button"));
-        for(WebElement okButton:buttonList){
-            if(okButton.getText().compareToIgnoreCase("ok") == 0){
+        for (WebElement okButton : buttonList) {
+            if (okButton.getText().compareToIgnoreCase("ok") == 0) {
                 okButton.click();
                 isSuccessful = !isQueuePresent(qName);  // if Queue present failure
                 break;
@@ -75,12 +75,20 @@ public class QueuesBrowsePage {
     }
 
 
-    private WebElement getTableRowByQueueName(final String qName){
-        WebElement workArea = driver.findElement(By.id(UIElementMapper.getInstance().getElement("mb.queue.list.page.workarea.id")));
-        List<WebElement> rowElementList = workArea.findElements(By.tagName("tr"));
+    private WebElement getTableRowByQueueName(final String qName) {
+
+        // if no queues available return null
+        if (driver.findElement(By.id(UIElementMapper.getInstance()
+                .getElement("mb.queue.list.page.workarea.id"))).getText().contains("No queues are created")) {
+
+            return null;
+        }
+
+        WebElement queueTable = driver.findElement(By.xpath(UIElementMapper.getInstance().getElement("mb.queue.list.table.body.xpath")));
+        List<WebElement> rowElementList = queueTable.findElements(By.tagName("tr"));
 
         // go through table rows and find the queue
-        for(WebElement row:rowElementList) {
+        for (WebElement row : rowElementList) {
             List<WebElement> columnList = row.findElements(By.tagName("td"));
             // Assumption: there are six columns. Delete buttons are in the sixth column
             if ((columnList.size() == 6) && columnList.get(0).getText().equals(qName)) {
