404b37a35d949931283963000a99a4029794f277
Akalanka <akalankapagoda@gmail.com>
2014-12-19 18:58:41 +0530

    Change commons-lang version.

--- modules/samples/product/mqtt_IoTSample/src/main/java/org/wso2/sample/mqtt/Main.java
+++ modules/samples/product/mqtt_IoTSample/src/main/java/org/wso2/sample/mqtt/Main.java
@@ -18,7 +18,7 @@
 
 package org.wso2.sample.mqtt;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.paho.client.mqttv3.MqttException;
