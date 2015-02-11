5856db9e2e29f7ab89eed29f978591edacd2b9d7
pranavan <pranavan.11@cse.mrt.ac.lk>
2015-02-11 11:02:10 +0530

    Test Cases Fixed Durable

--- modules/integration/tests-integration/tests-amqp/src/test/java/org/wso2/mb/integration/tests/amqp/functional/DifferentSubscriptionsWithDurableTopicTestCase.java
+++ modules/integration/tests-integration/tests-amqp/src/test/java/org/wso2/mb/integration/tests/amqp/functional/DifferentSubscriptionsWithDurableTopicTestCase.java
@@ -20,129 +20,128 @@ public class DifferentSubscriptionsWithDurableTopicTestCase {
         Integer runTime = 20;
         Integer expectedCount = 500;
 
-        String topicName = "a.b.c";
-        String hierarchicalTopic = "a.b.*";
+        String topicName = "ab.bc.cd";
+        String hierarchicalTopic = "ab.bc.*";
 
         // Start durable subscription 1
         AndesClient durableTopicSub1 = new AndesClient("receive", "127.0.0.1:5672",
-                                                       "topic:" + topicName,
-                                                       "100", "false", runTime.toString(),
-                                                       expectedCount.toString(),
-                                                       "1",
-                                                       "listener=true,ackMode=1,durable=true," +
-                                                       "subscriptionID=sub1,delayBetweenMsg=0," +
-                                                       "stopAfter=" + expectedCount, "");
+                "topic:" + topicName,
+                "100", "false", runTime.toString(),
+                expectedCount.toString(),
+                "1",
+                "listener=true,ackMode=1,durable=true," +
+                        "subscriptionID=sub1,delayBetweenMsg=0," +
+                        "unsubscribeAfter=" + expectedCount, "");
         durableTopicSub1.startWorking();
 
         // Start durable subscription 2
         AndesClient durableTopicSub2 = new AndesClient("receive", "127.0.0.1:5672",
-                                                       "topic:" + topicName,
-                                                       "100", "false", runTime.toString(),
-                                                       expectedCount.toString(),
-                                                       "1",
-                                                       "listener=true,ackMode=1,durable=true," +
-                                                       "subscriptionID=sub2,delayBetweenMsg=0," +
-                                                       "stopAfter=" + expectedCount, "");
+                "topic:" + topicName,
+                "100", "false", runTime.toString(),
+                expectedCount.toString(),
+                "1",
+                "listener=true,ackMode=1,durable=true," +
+                        "subscriptionID=sub2,delayBetweenMsg=0," +
+                        "unsubscribeAfter=" + expectedCount, "");
         durableTopicSub2.startWorking();
 
         //start a normal topic subscriber
         AndesClient normalTopicSub = new AndesClient("receive", "127.0.0.1:5672",
-                                                     "topic:" + topicName,
-                                                     "100", "false", runTime.toString(),
-                                                     expectedCount.toString(),
-                                                     "1",
-                                                     "listener=true,ackMode=1,durable=false," +
-                                                     "delayBetweenMsg=0," +
-                                                     "stopAfter=" + expectedCount, "");
+                "topic:" + topicName,
+                "100", "false", runTime.toString(),
+                expectedCount.toString(),
+                "1",
+                "listener=true,ackMode=1,durable=false," +
+                        "delayBetweenMsg=0," +
+                        "unsubscribeAfter=" + expectedCount, "");
         normalTopicSub.startWorking();
 
         //start a hierarchical normal topic subscriber
         AndesClient normalHierarchicalTopicSub = new AndesClient("receive", "127.0.0.1:5672",
-                                                                 "topic:" + hierarchicalTopic,
-                                                                 "100", "false", runTime.toString(),
-                                                                 expectedCount.toString(),
-                                                                 "1",
-                                                                 "listener=true,ackMode=1," +
-                                                                 "durable=false," +
-                                                                 "delayBetweenMsg=0," +
-                                                                 "stopAfter=" + expectedCount, "");
+                "topic:" + hierarchicalTopic,
+                "100", "false", runTime.toString(),
+                expectedCount.toString(),
+                "1",
+                "listener=true,ackMode=1," +
+                        "durable=false," +
+                        "delayBetweenMsg=0," +
+                        "stopAfter=" + expectedCount, "");
         normalHierarchicalTopicSub.startWorking();
 
         //start a hierarchical durable topic subscriber
         AndesClient durableHierarchicalTopicSub = new AndesClient("receive", "127.0.0.1:5672",
-                                                                  "topic:" + hierarchicalTopic,
-                                                                  "100", "false",
-                                                                  runTime.toString(),
-                                                                  expectedCount.toString(),
-                                                                  "1",
-                                                                  "listener=true,ackMode=1," +
-                                                                  "durable=true," +
-                                                                  "subscriptionID=sub3," +
-                                                                  "delayBetweenMsg=0," +
-                                                                  "stopAfter=" + expectedCount, "");
+                "topic:" + hierarchicalTopic,
+                "100", "false",
+                runTime.toString(),
+                expectedCount.toString(),
+                "1",
+                "listener=true,ackMode=1," +
+                        "durable=true," +
+                        "subscriptionID=sub3," +
+                        "delayBetweenMsg=0," +
+                        "stopAfter=" + expectedCount, "");
         durableHierarchicalTopicSub.startWorking();
 
         //start a queue subscriber
         AndesClient queueSubscriber = new AndesClient("receive", "127.0.0.1:5672",
-                                                      "queue:" + topicName,
-                                                      "100", "false", runTime.toString(),
-                                                      expectedCount.toString(),
-                                                      "1",
-                                                      "listener=true,ackMode=1,durable=false," +
-                                                      "delayBetweenMsg=0," +
-                                                      "stopAfter=" + expectedCount, "");
+                "queue:" + topicName,
+                "100", "false", runTime.toString(),
+                expectedCount.toString(),
+                "1",
+                "listener=true,ackMode=1,durable=false," +
+                        "delayBetweenMsg=0," +
+                        "stopAfter=" + expectedCount, "");
         queueSubscriber.startWorking();
 
 
         // Start message publisher
         AndesClient sendingClient = new AndesClient("send", "127.0.0.1:5672",
-                                                    "topic:" + topicName,
-                                                    "100", "false",
-                                                    runTime.toString(), sendCount.toString(), "1",
-                                                    "ackMode=1,delayBetweenMsg=0," +
-                                                    "stopAfter=" + sendCount,
-                                                    "");
+                "topic:" + topicName,
+                "100", "false",
+                runTime.toString(), sendCount.toString(), "1",
+                "ackMode=1,delayBetweenMsg=0," +
+                        "stopAfter=" + sendCount,
+                "");
         sendingClient.startWorking();
 
         AndesClientUtils.sleepForInterval(4000);
 
         boolean durableTopicSub1Success = AndesClientUtils
                 .waitUntilMessagesAreReceived(durableTopicSub1, expectedCount,
-                                              runTime);
+                        runTime);
         assertTrue(durableTopicSub1Success, "Message receive error from durable subscriber 1");
 
         boolean durableTopicSub2Success = AndesClientUtils
                 .waitUntilMessagesAreReceived(durableTopicSub2, expectedCount,
-                                              runTime);
+                        runTime);
 
         assertTrue(durableTopicSub2Success, "Message receive error from durable subscriber 2");
 
         boolean normalTopicSubSuccess = AndesClientUtils
                 .waitUntilMessagesAreReceived(normalTopicSub, expectedCount,
-                                              runTime);
+                        runTime);
         assertTrue(normalTopicSubSuccess, "Message receive error from normal topic subscriber");
 
         boolean normalHierarchicalTopicSubSuccess = AndesClientUtils
                 .waitUntilMessagesAreReceived(normalHierarchicalTopicSub, expectedCount,
-                                              runTime);
+                        runTime);
         assertTrue(normalHierarchicalTopicSubSuccess,
-                   "Message receive error from normal hierarchical topic subscriber");
+                "Message receive error from normal hierarchical topic subscriber");
 
         boolean durableHierarchicalTopicSubSuccess = AndesClientUtils
                 .waitUntilMessagesAreReceived(durableHierarchicalTopicSub, expectedCount,
-                                              runTime);
+                        runTime);
         assertTrue(durableHierarchicalTopicSubSuccess,
-                   "Message receive error from durable hierarchical topic subscriber");
+                "Message receive error from durable hierarchical topic subscriber");
 
         boolean queueSubscriberSuccess = AndesClientUtils
                 .waitUntilMessagesAreReceived(queueSubscriber, expectedCount,
-                                              runTime);
+                        runTime);
         assertFalse(queueSubscriberSuccess,
-                    "Message received from queue subscriber. This should not happen");
+                "Message received from queue subscriber. This should not happen");
         AndesClientUtils.sleepForInterval(2000);
 
         boolean sendingSuccess = AndesClientUtils.getIfSenderIsSuccess(sendingClient, sendCount);
         assertTrue(sendingSuccess, "Message send error");
     }
 }
-
