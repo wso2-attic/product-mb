429e028b1165cc8a349a9ecfeee7cf1dfc48a46d
sajini de silva <sdesilva28@gmail.com>
2015-03-07 11:59:24 +0530

    Removing topic name validation against colon test case added

--- modules/integration/tests-common/integration-tests-utils/src/main/java/org/wso2/mb/integration/common/utils/ui/pages/main/QueuesBrowsePage.java
+++ modules/integration/tests-common/integration-tests-utils/src/main/java/org/wso2/mb/integration/common/utils/ui/pages/main/QueuesBrowsePage.java
@@ -27,7 +27,7 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- * This page represents 'Queues-> Browse' page in MB management console.
+ * This page represents 'Topic-> Browse' page in MB management console.
  */
 public class QueuesBrowsePage {
 
