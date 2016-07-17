78a63447db8ff2404ef65cabe5f46162185b4621
Hemika Kodikara <hemika.kodikara@gmail.com>
2016-07-17 19:12:42 +0530

    Added test cases for per message ack and redelivery delay. Adding samples for per message ack and redelivery delay.

---
+++ modules/samples/product/PerMessageAcknowledgeSample/src/org/sample/jms/Main.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2016 WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sample.jms;
+
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.naming.NamingException;
+
+/**
+ * Sample executor class for per message acknowledgements.
+ */
+public class Main {
+    public static void main(String[] args) throws NamingException, JMSException {
+        // AndesAckWaitTimeOut is the timeout value till reject is sent
+        System.setProperty("AndesAckWaitTimeOut", "4000");
+
+        // Create subscriber
+        SamplePerMessageAckSubscriber queueReceiver = new SamplePerMessageAckSubscriber();
+        MessageConsumer consumer = queueReceiver.registerSubscriber();
+        // Create publisher
+        SampleQueueSenderForPerMessageAckSample queueSender = new SampleQueueSenderForPerMessageAckSample();
+
+        // Publishing messages
+        queueSender.sendMessages("#1");
+        queueSender.sendMessages("#2");
+        queueSender.sendMessages("#3");
+        queueSender.sendMessages("#4");
+        queueSender.sendMessages("#5");
+
+        // Stopping publisher
+        queueSender.stopClient();
+        System.out.println();
+
+        // Acking message #1
+        queueReceiver.receiveMessages(consumer, true);
+        // Not acking message #2
+        queueReceiver.receiveMessages(consumer, false);
+        // Not acking message #3
+        queueReceiver.receiveMessages(consumer, false);
+        // Acking message #4
+        queueReceiver.receiveMessages(consumer, true);
+        // Acking message #5
+        queueReceiver.receiveMessages(consumer, true);
+
+        System.out.println();
+
+        // Message #2 and #3 gets redelivered to the subscriber by the server as they were not acked.
+        // Acking message #2
+        queueReceiver.receiveMessages(consumer, true);
+        // Acking message #3
+        queueReceiver.receiveMessages(consumer, true);
+
+        System.out.println();
+
+        // Stopping subscriber
+        queueReceiver.stopClient(consumer);
+    }
+}
---
+++ modules/samples/product/RedeliveryDelaySample/src/org/sample/jms/SampleQueueSenderForDelayedRedeliverySample.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2016 WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sample.jms;
+
+import java.util.Properties;
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * A sample queue message publisher for per message ack sample.
+ */
+public class SampleQueueSenderForDelayedRedeliverySample {
+
+    public static final String QPID_ICF = "org.wso2.andes.jndi.PropertiesFileInitialContextFactory";
+    private static final String CF_NAME_PREFIX = "connectionfactory.";
+    private static final String QUEUE_NAME_PREFIX = "queue.";
+    private static final String CF_NAME = "qpidConnectionfactory";
+    String userName = "admin";
+    String password = "admin";
+    private static String CARBON_CLIENT_ID = "carbon";
+    private static String CARBON_VIRTUAL_HOST_NAME = "carbon";
+    private static String CARBON_DEFAULT_HOSTNAME = "localhost";
+    private static String CARBON_DEFAULT_PORT = "5672";
+    String queueName = "delayedDeliveryQueue";
+    private QueueConnection queueConnection;
+    private QueueSession queueSession;
+    private QueueSender queueSender;
+
+    /**
+     * Creates a queue publisher.
+     *
+     * @throws NamingException
+     * @throws JMSException
+     */
+    public SampleQueueSenderForDelayedRedeliverySample() throws NamingException, JMSException {
+        Properties properties = new Properties();
+        properties.put(Context.INITIAL_CONTEXT_FACTORY, QPID_ICF);
+        properties.put(CF_NAME_PREFIX + CF_NAME, getTCPConnectionURL(userName, password));
+        properties.put(QUEUE_NAME_PREFIX + queueName, queueName);
+        InitialContext ctx = new InitialContext(properties);
+        // Lookup connection factory
+        QueueConnectionFactory connFactory = (QueueConnectionFactory) ctx.lookup(CF_NAME);
+        queueConnection = connFactory.createQueueConnection();
+        queueConnection.start();
+        queueSession = queueConnection.createQueueSession(false, QueueSession.AUTO_ACKNOWLEDGE);
+        // Send message
+        Queue queue = (Queue) ctx.lookup(queueName);
+        queueSender = queueSession.createSender(queue);
+    }
+
+    /**
+     * Publishes a message with given content.
+     *
+     * @param messageContent The message content.
+     * @throws JMSException
+     */
+    public void sendMessages(String messageContent) throws JMSException {
+        System.out.println("Publishing message==>" + messageContent);
+        TextMessage textMessage = queueSession.createTextMessage(messageContent);
+        queueSender.send(textMessage);
+    }
+
+    /**
+     * Shutdown the publisher client.
+     *
+     * @throws JMSException
+     */
+    public void stopClient() throws JMSException {
+        queueSender.close();
+        queueSession.close();
+        queueConnection.close();
+    }
+
+    /**
+     * Creates amqp url.
+     *
+     * @param username The username for the amqp url.
+     * @param password The password for the amqp url.
+     * @return AMQP url.
+     */
+    private String getTCPConnectionURL(String username, String password) {
+        // amqp://{username}:{password}@carbon/carbon?brokerlist='tcp://{hostname}:{port}'
+        return new StringBuffer()
+                .append("amqp://")
+                .append(username)
+                .append(":")
+                .append(password)
+                .append("@")
+                .append(CARBON_CLIENT_ID)
+                .append("/")
+                .append(CARBON_VIRTUAL_HOST_NAME)
+                .append("?brokerlist='tcp://").append(CARBON_DEFAULT_HOSTNAME)
+                .append(":")
+                .append(CARBON_DEFAULT_PORT)
+                .append("'").toString();
+    }
+}
